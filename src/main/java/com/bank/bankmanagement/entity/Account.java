6547516e808a602ec5e4bbaff5004fc15d0bac85
yassine4real <yssnabbou2002@gmail.com>
2024-12-29 02:29:17 +0100

    create Account entity

--- src/main/java/com/bank/bankmanagement/entity/Account.java
+++ src/main/java/com/bank/bankmanagement/entity/Account.java
@@ -1,7 +1,27 @@
 package com.bank.bankmanagement.entity;
 
-/**
- * @author $ {USER}
- **/
+
+import jakarta.persistence.*;
+
+@Entity
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+    private String ownerName;
+    @Column(nullable = false)
+    private Double balance;
+    // Getters et setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getAccountNumber() { return accountNumber; }
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber; }
+    public String getOwnerName() { return ownerName; }
+    public void setOwnerName(String ownerName) { this.ownerName =
+            ownerName; }
+    public Double getBalance() { return balance; }
+    public void setBalance(Double balance) { this.balance = balance; }
 }
